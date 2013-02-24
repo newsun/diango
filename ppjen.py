cbb38e07f796678e7597404007c24a0ba68523c4
kejiang <kejiang@paypal.com>
2013-02-24 11:26:30 -0800

    Signed-off-by: kejiang <kejiang@paypal.com>

--- ppjen.py
+++ ppjen.py
@@ -1,5 +1,9 @@
 import logging
 from jenkinsapi.jenkins import Jenkins
+from template import *
+from optparse import OptionParser
+import re
+import ConfigParser
 #try:
 #	from jenkinsapi.jenkins import Jenkins
 #except ImportError:
@@ -21,11 +25,14 @@ logger=logging.getLogger()
 logger.addHandler(console)
 ###############################################
 jen_url = "https://fusion.paypal.com/jenkins/"
-username = "kejiang"
-password = "Symbio@2013"
-jen = Jenkins(jen_url,username,password)
+jen = None
+jobs = []
 
-def chain(jobsName,doChain=False):
+def joblist(jobsName):
+    assert isinstance(jobsName,list)
+    jobs.append(jobsName)
+    
+def chain(jobsName,dochain=True):
     assert isinstance(jobsName,list)
     jobsName.sort()
     jobsName.reverse()
@@ -34,60 +41,109 @@ def chain(jobsName,doChain=False):
         job = jen[jobName]
         job.modify_chain(chain)
         logger.info("%s is chained to %s"%(chain,jobName))
-        chain = doChain and jobName or chain
+        chain = dochain and jobName or chain
 
-def goals(jobsName,newStr,oldStr=None,count=-1):
-    goalsStr = "clean test -DstageName=${stageName} -DBLUEFIN_SELENIUM_HOST=10.57.88.98 -DBLUEFIN_HOSTNAME=${stageName}.${stageDomain}.paypal.com -DBLUEFIN_SSH_USER=${SSH_USER} -DJAWS_DEFAULT_EMAIL_PREFIX=${DEFAULT_EMAIL_PREFIX} -DJAWS_NIGHTOWL_MAIL_SERVER=nightowllvs01.qa.paypal.com -DsuiteXmlFile=src/test/resources/configfiles/LqaRegressionTesting/%s/%s.xml -Dpaypal.buildid=5333310"
+def goals(jobsName,newStr=None,oldStr=None,count=-1):
     assert isinstance(jobsName,list)
+    goalsStr = "clean test -DstageName=${stageName} -DBLUEFIN_SELENIUM_HOST=10.57.88.98 -DBLUEFIN_HOSTNAME=${stageName}.${stageDomain}.paypal.com -DBLUEFIN_SSH_USER=${SSH_USER} -DJAWS_DEFAULT_EMAIL_PREFIX=${DEFAULT_EMAIL_PREFIX} -DJAWS_NIGHTOWL_MAIL_SERVER=nightowllvs01.qa.paypal.com -DsuiteXmlFile=%s -Dpaypal.buildid=5333310"
     jobsName.sort()
     for jobName in jobsName:
-        locale = jobName[3:7]
         job = jen[jobName]
-        job.modify_goals(newStr,oldStr)
-        logger.info("%s's goal is updated"%jobName)
+        job.modify_goals(goalsStr%template[jobName.endswith("_Debug") and jobName[:-6] or jobName])
+        logger.info("goals of %s has been updated"%jobName)
         
 def modify_view_jobs(url,fn,*args, **kwargs):
     view = jen.get_view_by_url(url)
     jobsName = view.get_jobs_list()
-#    args_ = args.append(jobsName)
     fn(jobsName,*args, **kwargs)
     subviews = view.get_view_dict()
     for vn,vu in subviews.iteritems():
 #        view = fv.get_view(vn)
         modify_view_jobs(vu,fn,*args, **kwargs)
 
-def launch_all_flows():
-    pass
+def launch(jobsName,flow,stage,email,sshUser="ppbuild",domain="qa"):
+    assert isinstance(jobsName,list)
+    if len(jobsName)==0:
+        return
+    if re.match("\d+_\w+",flow):
+        flow = flow[:3]
+    if jobsName[0].lower().find(flow.lower())<0 :
+        return
+    jobsName.sort()
+    jobName = jobsName[0]
+    job = jen[jobName]
+    params = {"stageName":stage,"SSH_USER":sshUser,"DEFAULT_EMAIL_PREFIX":email,"stageDomain":domain}
+    job.invoke(params=params)
+#    logger.info("Job %s has started"%jobName)
+    
+def launchall(jobsName, file):
+    raise Exception("Under construction")
+    assert isinstance(jobsName,list)
+    try:
+        from fileName import params
+    except:
+        raise Exception("file % doesn't exist")
+        logger.info("Failed to lauch all flows")
+    jobsName.sort()
+    jobName = jobsName[0]
+    flowName = jobName[:2]+jobName[7:]
+    job = jen[jobName]
+    job.invoke(params = params[flowName])
+    
 
 if __name__=='__main__':
-    username = "kejiang"
-    password = "Symbio@2013"
-    url = "https://fusion.paypal.com/jenkins/user/tkhoo/my-views/view/Flow%20View"
+#username = "kejiang"
+#password = "Symbio@2013"
+#url = "https://fusion.paypal.com/jenkins/view/InternationalQA_View/view/Horizontally%20chained%20view%20%28under%20construction%29/view/Per%20Flow/"
+#jen = Jenkins(jen_url,username,password)
 #    modify_view_jobs(url,chain,doChain=False)
-    modify_view_jobs(url,goals,"-Dpaypal.buildid=5333310","-Dpaypal.buildid=\d+")
+#    modify_view_jobs(url,goals,"-Dpaypal.buildid=5333310","-Dpaypal.buildid=\d+")"
+    usage = """
+    This programe is to modify all the jobs under a given view and its sub views.
+    python %prog [option][value]...
+    Usage 1: python ppjen.py -u usename -p password -l viewurl -a chain [-c True|False]
+    Usage 2: python ppjen.py -u usename -p password -l viewurl -a goals  [-n newStr [-o oldStr]]
+    Usage 2: python ppjen.py -u usename -p password -l viewurl -a launch  [-s stage -e email]
+    """
+    parser = OptionParser(usage=usage,version="%prog 1.0")
+    parser.add_option("-u","--username",dest="username",help="user name to login jenkins")
+    parser.add_option("-p","--password",dest="password",help="user password to login jenkins")
+    parser.add_option("-l","--url",dest="url",help="the view's url under which the jobs you want to update")
+    parser.add_option("-a","--action",dest="action",help="the action you want to execute, valid values: [goals: update job's goals; chain: chain or unchain the jobs alphabetically; launch: launch a flow; launchAll: launch all flows")
+    parser.add_option("-c","--dochain",dest="dochain",help="chain or unchain the jobs under a view")
+    parser.add_option("-o","--oldStr",dest="oldStr",help="the old string (or a wildword expression) going to replace by new string, optioanl, default None")
+    parser.add_option("-n","--newStr",dest="oldStr",help="the new string going to replace the oldstring, optional, default None")
+    parser.add_option("-w","--flow",dest="flow",help="the flow name which is going to be launched")
+    parser.add_option("-s","--stage",dest="stage",help="the stage for invoking a job")
+    parser.add_option("-e","--email",dest="email",help="the email for invoking a job")
+    parser.add_option("-r","--sshuser",dest="ppuser",help="the ppuser for invoking a job")
+    parser.add_option("-d","--domain",dest="domain",help="the stage domain for invoking a job")
+    parser.add_option("-f","--file",dest="file",help="the configuration file")
+    options,args = parser.parse_args()
     
-    
-#	if jen.login():
-#		logger.info(" %s login successfully"%username)
-	#view = jen.get_view_by_url('https://fusion.paypal.com/jenkins/user/tkhoo/my-views/view/Flow%20View/view/Flow%2001/')
-#	for fn in flows[:]:
-#		viewURL = "https://fusion.paypal.com/jenkins/view/InternationalQA_View/view/Horizontally%20chained%20view%20%28under%20construction%29/view/Per%20Flow/view/"+fn
-#		view = jen.get_view_by_url(viewURL)
-#		jobNames = view.get_job_dict().keys()
-#		jobNames.sort()
-#		print jobNames[0]
-#		jen[jobNames[0]].invoke()		
-#		break
-#		jobNames.reverse()
-#		chain = None
-#		#job=jen[jobNames[0]]
-#		for jn in jobNames:
-#			if jn.find("jaJP")>0:
-#				continue
-#			#job = jen[jn]
-#			#job.modify_chain(chain)
-#			#job.modify_goals("","${DEFAULT_EMAIL_PREFIX_}")
-#			#logger.info("%s is chained to %s"%(chain,jn))
-#			chain=jn
-#			#break
-	
+#    if options.file:
+#        config.readfp(open(options.file,'rb'))
+    if not options.username:
+        parser.error("username can't be empty")
+    if not options.url:
+        parser.error("view url can't be empty")
+    if not options.password:
+        parser.error("password can't be empty")
+    if not options.action:
+        parser.error("action can't be empty")
+    if options.action == "launch" and (not options.flow or not options.stage or not options.email):
+        parser.error("lauch action needs flow name, stage and email")
+    if options.action == "launchall" and (not options.file):
+        parser.error("lauchall action needs configuration file")
+
+    jen = Jenkins(jen_url,options.username,options.password)
+    if options.action == "launch":
+        modify_view_jobs(options.url,eval(options.action),options.flow,options.stage,options.email)  
+    elif options.action == "launchall":
+        modify_view_jobs(options.url,eval(options.action),options.file)
+    elif options.action == "chain":
+        modify_view_jobs(options.url,eval(options.action),options.dochain)
+    elif options.action == "goals":
+        modify_view_jobs(options.url,eval(options.action),options.newStr,options.oldStr)
+    else:
+        pass
