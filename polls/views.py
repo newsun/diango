a18112504fb870261cb886b225483c2ab637c9a9
kejiang <kejiang@paypal.com>
2013-03-17 17:53:38 -0700

    Signed-off-by: kejiang <kejiang@paypal.com>

---
+++ polls/views.py
@@ -0,0 +1,36 @@
+# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.template import Context,loader
+
+from polls.models import Poll
+
+#def index(request):
+#    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+#    template = loader.get_template('polls/index.html')
+#    context = Context({
+#        'latest_poll_list': latest_poll_list,
+#    })
+#    return HttpResponse(template.render(context))
+
+def index(request):
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    context = {'latest_poll_list': latest_poll_list}
+    return render(request,'polls/index.html',context)
+
+#def detail(request, poll_id):
+#    try:
+#        poll = Poll.objects.get(pk=poll_id)
+#    except Poll.DoesNotExist:
+#        raise Http404
+#    return render(request, 'polls/detail.html', {'poll': poll})
+
+def detail(request, poll_id):
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/detail.html', {'poll': poll})
+
+def results(request, poll_id):
+    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+
+def vote(request, poll_id):
+    return HttpResponse("You're voting on poll %s." % poll_id)
